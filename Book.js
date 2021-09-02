9d7cf9cdf4aa57fe8a46cf4291d5e8796d8e1854
almahfuz2019 <almahfuz2019@gmail.comm>
2021-09-02 19:32:05 +0600

    2nd commit

--- Book.js
+++ Book.js
@@ -1,49 +1,67 @@
+const inputFiled = document.getElementById('input-Filed');
+const totalBook = document.getElementById('total-book');
+const error = document.getElementById('error-message').style.display = 'none';
 const searchBox = () => {
-    const inputFiled = document.getElementById('input-Filed');
-    const searchResult = inputFiled.value;
 
-    console.log(searchResult);
-    inputFiled.value = '';
 
-    // console.log(search)
+    // const url = `https://openlibrary.org/search.json?q=${searchResult}`
+    // fetch(url)
+    //     .then(res => res.json())
+    //     .then(data => displyUi(data.docs))
+    // inputFiled.value = '';
 
+    if (inputFiled.value == '') {
+
+        document.getElementById('error-message').style.display = 'block';
+        console.log(4)
+
+    } else {
+        const searchResult = inputFiled.value;
+        const url = `https://openlibrary.org/search.json?q=${searchResult}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displyUi(data))
+        inputFiled.value = '';
+    }
 }
-
-
-const loadSingleUser = () => {
-    const url = `https://openlibrary.org/search.json?q=${searchResult}`
-
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displyUi(data))
-
-}
-
-
 
 const displyUi = dUi => {
-    console.log(dui);
-    const allbooks = document.getElementById('all-Book');
-    dUi.forEach(book => {
-        const Div = document.createElement('div');
-
-        Div.innerHTML = `
-
-    <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${book.title}</h5>
-                    <p class="card-text">${book.num_found}</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
+        totalBook.innerText = `${dUi.numFound}`
+        console.log(dUi)
+        const allbooks = document.getElementById('all-Book');
+        allbooks.textContent = '';
+        dUi.docs.forEach(book => {
+            const div = document.createElement('div');
+            div.innerHTML = `
+            <div class="col">
+            <div class="card full-card bg-light border border-1 shadow-sm">
+                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img-fluid imageSize" alt="...">
+                <div class="p-1">
+                <p class="card-title fontSize">Book Name: ${book.title}</p>
+                    <p class="card-text fontSize">Writer: ${book.author_name} </p>
+                    <p class="card-text fontSize">Publisher:${book.edition_count} </p>
+                    <p class="card-text fontSize">edition_count:First Publish Year:${book.first_publish_year} </p>
+                    
                 </div>
             </div>
-    `
-        allbooks.appendChild(Div);
+        </div>
 
-    });
+                    `
+            allbooks.appendChild(div);
 
+        });
+    }
+    // error
 
-
-}
+// <div class="card"> 
+// <
+//     div class = "card-body" >
+//     <
+//     img src = "https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg"
+// class = "card-img-top"
+// alt = "..." >
+//     <
+//     h5 class = "card-title" > $ { book.title } < /h5> <
+//     p class = "card-text" > $ { book.author_name } < /p> <
+//     /div> <
+//     /div>
