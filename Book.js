339ef65bc342c9411dfc334f4df9470f623f0e8f
almahfuz2019 <almahfuz2019@gmail.comm>
2021-09-02 22:35:06 +0600

    3rd

--- Book.js
+++ Book.js
@@ -1,19 +1,13 @@
 const inputFiled = document.getElementById('input-Filed');
-const totalBook = document.getElementById('total-book');
+const totalBookitems = document.getElementById('total-book-items');
 const error = document.getElementById('error-message').style.display = 'none';
+const searchError = document.getElementById('search-error');
 const searchBox = () => {
-
-
-    // const url = `https://openlibrary.org/search.json?q=${searchResult}`
-    // fetch(url)
-    //     .then(res => res.json())
-    //     .then(data => displyUi(data.docs))
-    // inputFiled.value = '';
 
     if (inputFiled.value == '') {
 
         document.getElementById('error-message').style.display = 'block';
-        console.log(4)
+        // console.log(4)
 
     } else {
         const searchResult = inputFiled.value;
@@ -25,43 +19,35 @@ const searchBox = () => {
     }
 }
 
-const displyUi = dUi => {
-        totalBook.innerText = `${dUi.numFound}`
-        console.log(dUi)
+const displyUi = display => {
+
+    if (display.docs.length === 0) {
+        searchError.innerHTML = `Your search - didn't match our books. Please enter a
+    correct name.`
+    } else {
+        totalBookitems.innerText = `Total Result-${display.numFound}`
+            // console.log(display)
         const allbooks = document.getElementById('all-Book');
         allbooks.textContent = '';
-        dUi.docs.forEach(book => {
+        display.docs.forEach(book => {
             const div = document.createElement('div');
             div.innerHTML = `
-            <div class="col">
-            <div class="card full-card bg-light border border-1 shadow-sm">
-                <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img-fluid imageSize" alt="...">
-                <div class="p-1">
-                <p class="card-title fontSize">Book Name: ${book.title}</p>
-                    <p class="card-text fontSize">Writer: ${book.author_name} </p>
-                    <p class="card-text fontSize">Publisher:${book.edition_count} </p>
-                    <p class="card-text fontSize">edition_count:First Publish Year:${book.first_publish_year} </p>
-                    
+                <div class="col">
+                <div class="card full-card bg-light border border-1 shadow-sm">
+                    <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img-fluid imageSize" alt="...">
+                    <div class="p-1">
+                    <p class="card-title fontSize">Book Name: ${book?.title}</p>
+                        <p class="card-text fontSize">Writer: ${book?.author_name}</p>
+                        <p class="card-text fontSize">edition_count:First Publish Year:${book?.first_publish_year} </p>
+                        
+                    </div>
                 </div>
             </div>
-        </div>
-
-                    `
+    
+                        `
             allbooks.appendChild(div);
 
         });
     }
-    // error
 
-// <div class="card"> 
-// <
-//     div class = "card-body" >
-//     <
-//     img src = "https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg"
-// class = "card-img-top"
-// alt = "..." >
-//     <
-//     h5 class = "card-title" > $ { book.title } < /h5> <
-//     p class = "card-text" > $ { book.author_name } < /p> <
-//     /div> <
-//     /div>
+}
